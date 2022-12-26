a4ef879f5462a1f40a44173a9e8ef2b7f7cc2f36
￼Marcio-Gabriel-Roque-Mendes <marcio_2411@hotmail.com>
2022-12-26 12:13:48 -0300

    Feat: mudando a API de requisição do jogo para The Trivia Api e removendo a api geradora de token

--- src/services/fethApiTrivia.js
+++ src/services/fethApiTrivia.js
@@ -1,16 +1,8 @@
-const URL_TOKEN = 'https://opentdb.com/api_token.php?command=request';
-
-const getToken = async () => {
-  const data = await fetch(URL_TOKEN);
-  const dataJson = await data.json();
-  return dataJson.token;
-};
-
-const getQuestion = async (token) => {
-  const URL_QUESTION = `https://opentdb.com/api.php?amount=5&token=${token}`;
+const getQuestion = async () => {
+  const URL_QUESTION = `https://the-trivia-api.com/api/questions?limit=5`;
   const data = await fetch(URL_QUESTION);
   const dataJson = await data.json();
   return dataJson;
 };
 
-export { getToken, getQuestion };
+export { getQuestion };
